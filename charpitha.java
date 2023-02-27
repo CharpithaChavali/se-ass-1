953b73bde8cc9353a38692091d3db3b42db8235e
CharpithaChavali <111104653+CharpithaChavali@users.noreply.github.com>
2023-02-27 23:35:42 +0530

    Update charpitha.java

--- charpitha.java
+++ charpitha.java
@@ -1,5 +1,6 @@
 class charpitha{
     public static void main(String[] args) {
-        system.out.println("this is my first commit");
+        System.out.println("this is my first commit");
+        System.out.println("this is my second commit");
     }
 }
